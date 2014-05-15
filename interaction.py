297b4bc6e23ee11607249917f7bd125600cd627e
Russell Davies <russell@zeroflux.net>
2014-05-15 14:33:59 +0100

    Finished list_cards method

--- interaction.py
+++ interaction.py
@@ -74,32 +74,37 @@ class Account(object):
     @login_required
     def list_cards(self):
         """
-        Returns the cards a user has registered in a dict in 
-        the form 'card name: card id'.
+        Return the cards a user has registered as a dict in
+        the form {card_id: {card details}}.
         """
         url = BASE_URL + "/en/SelfServices/CardServices/CardOverView.aspx"
+        event_target = 'ctl00$ctl00$ContentPlaceHolder1$TabContainer2$MyCardsTabPanel$ddlMyCardsList'
 
         self.br.open(url)
         soup = BeautifulSoup(self.br.response().read())
-        card_tags = soup.find('select').find_all('option')
-        for tag in card_tags:
-            tag.has_attr('selected')
 
-        # Card overview details
-        #[list(tag.stripped_strings) for tag in soup.find(id=re.compile('CardDetails')).find_all('li')[:-1]]
-        overview_vals = [tag.contents[2].strip() for tag in
-                soup.find(id=re.compile('CardDetails')).find_all('li')]
-        overview_keys = ('number', 'label', 'type', 'status', 'credit_status',
-                'auto_topup', 'init_date', 'expiry_date', 'balance')
-        card_overview = zip(overview_keys, overview_vals)
+        cards = {}
+        for tag in soup.find('select').find_all('option'):
+            card_id = tag['value']
+            card_name = tag.text
+            if tag.has_attr('selected'):
+                cards[card_id] = self._card_overview(soup)
+            else:
+                self.br.select_form(nr=0)
+                self.br.set_all_readonly(False)
+                self.br["__EVENTTARGET"] = event_target
+                self.br[event_target] = [card_id]
+                self.br.submit()
+                soup = BeautifulSoup(self.br.response().read())
+                cards[card_id] = self._card_overview(soup)
 
-
-        return {tag['value']: tag.text for tag in card_tags}
+        return cards
 
     def card_history(self, card_id):
         """Returns a list of all the journeys for a specific card."""
-        
+
         url = BASE_URL + "/en/SelfServices/CardServices/ViewJourneyHistory.aspx"
+        import ipdb; ipdb.set_trace()
         cardlist_control = "ctl00$ContentPlaceHolder1$ddlUserRegisteredCards"
 
         self.br.open(url)
@@ -140,3 +145,16 @@ class Account(object):
                 'balance': cols[5].string.strip(),
             })
         return journeys
+
+    def _card_overview(self, soup):
+        """
+        Return a dict of card details.
+        """
+        #[list(tag.stripped_strings) for tag in soup.find(id=re.compile('CardDetails')).find_all('li')[:-1]]
+        overview_vals = [tag.contents[2].strip() for tag in
+                soup.find(id=re.compile('CardDetails')).find_all('li')[:-1]]
+        overview_keys = ('number', 'label', 'type', 'status', 'credit_status',
+                'auto_topup', 'init_date', 'expiry_date', 'balance')
+        card_overview = dict(zip(overview_keys, overview_vals))
+
+        return card_overview
